179990691ed5917beefd6af0dc2ce6a795b53c1e
Ben Ropa <benropa@outlook.com>
2023-02-26 13:51:54 -0600

    adding doms to javascipr file

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ var searchHistory = [];
 var weatherApiRootUrl = 'https://api.openweathermap.org';
 var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
 
+// Document Object Model refs
+var searchForm = document.querySelector('#search-form');
+var searchInput = document.querySelector('#search-input');
+var todayContainer = document.querySelector('#today');
+var forecastContainer = document.querySelector('#forecast');
+var searchHistoryContainer = document.querySelector('#history');
+
