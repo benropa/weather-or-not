2d18c82b7d62a5eda3a63eff080fe0b1b11ed946
Ben Ropa <benropa@outlook.com>
2023-03-12 17:32:05 -0500

    adding basic html css and javascript to test

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-// Declaring Global Variables
-var searchHistory = [];
-var weatherApiRootUrl = 'https://api.openweathermap.org';
-var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
+let apiKey = 'd91f911bcf2c0f925fb6535547a5ddc9'; 
 
+const searchForm = document.querySelector('form');
+const searchInput = document.querySelector('#search-input');
+const weatherInfo = document.querySelector('#weather-info');
+
+searchForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const cityName = searchInput.value.trim();
+
+	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`)
+		.then(response => response.json())
+		.then(data => {
+			const temp = data.main.temp;
+			const description = data.weather[0].description;
+			const cityName = data.name;
+			const country = data.sys.country;
+			weatherInfo.innerHTML = `The temperature in ${cityName}, ${country} is ${temp}°C with ${description}.`;
+		})
+		.catch(error => {
+			console.log(error);
+			weatherInfo.innerHTML = `Error: Could not find weather for ${cityName}.`;
+		});
+});
